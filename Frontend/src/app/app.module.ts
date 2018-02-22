57980fc7c949b0bea1c7531d3fe80e7ed05bb09f
Stuart Mathews <stuart.mathews@finbourne.com>
2018-02-22 17:31:13 +0000

    more pruning

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -7,15 +7,12 @@ import { OAuthModule } from 'angular-oauth2-oidc';
 import { HomeComponent } from './home/home.component';
 import { AuthGuard } from './shared';
 import { RouterModule, Routes } from '@angular/router';
-import { PortfoliosComponent } from './portfolios/portfolios.component';
 import { ApiService } from './apiService';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { UrlInterceptor as MyFirstInterceptor } from './UrlInterceptor';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BsDatepickerModule } from 'ngx-bootstrap';
-import { LayoutComponent } from './layout/layout.component';
-import { SelectionTestComponent } from './selection-test/selection-test.component';
 import { CollapseModule, BsDropdownModule } from 'ngx-bootstrap';
 import { CustomErrorHandler } from './ErrorCatcher';
 import {PortfolioComponent} from './Views/Portfolio/portfolio';
@@ -25,7 +22,6 @@ import {HoldingComponent} from './Views/Holding/holding';
 import { PortfolioGroupComponent} from './Views/PortfolioGroup/portfolio-group';
 import {PropertyTypeComponent} from './Views/PropertyType/property-type';
 import {ListTradesComponent } from './Views/Shared/list-trades';
-//import {ListPropertiesComponent} from './Views/Shared/list-properties';
 import {ListHoldingsComponent as ListGroupsComponent} from './Views/Shared/list-holdings';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { InlineEditorModule } from '@qontu/ngx-inline-editor';
@@ -44,14 +40,10 @@ const appRoutes: Routes = [
   { path: 'Security', component: SecurityComponent},
   { path: 'PropertyTypes', component: PropertyTypeComponent },
   { path: 'Property/:domain', component: PropertyComponent },
-  { path: 'layout', data: { breadcrumb: 'layout' }, component: LayoutComponent },
-  { path: 'selection-tests', data: { breadcrumb: 'selection' },component: SelectionTestComponent },
   { path: 'home', data: { breadcrumb: 'Home' }, component: HomeComponent, children: [] },
-  { path: 'portfolios', data: { breadcrumb: 'portfolios' }, component: PortfoliosComponent, canActivate: [AuthGuard], children: [
-      { path: '', data: { breadcrumb: 'All Portfolios' }, component: PortfolioComponent },
-      { path: 'Portfolios', data: { breadcrumb: 'All Portfolios' }, component: PortfolioComponent },
-      { path: 'PortfolioDetails/:id', data: { breadcrumb: 'Portfolios Details' }, component: PortfolioDetailComponent } ]
-  },
+  { path: '', data: { breadcrumb: 'All Portfolios' }, component: PortfolioComponent },
+  { path: 'Portfolios', data: { breadcrumb: 'All Portfolios' }, component: PortfolioComponent },
+  { path: 'PortfolioDetails/:id', data: { breadcrumb: 'Portfolios Details' }, component: PortfolioDetailComponent },
   { path: 'Trades/:id', data: { breadcrumb: 'Portfolios Trades' }, component: TradeComponent },
   { path: 'Holdings/:id', data: { breadcrumb: 'Portfolios Holdings' }, component: HoldingComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -60,7 +52,7 @@ const appRoutes: Routes = [
 
 @NgModule({
   declarations: [
-    AppComponent, HomeComponent, PortfoliosComponent, LayoutComponent, SelectionTestComponent,
+    AppComponent, HomeComponent,
     PortfolioComponent, TradeComponent, HoldingComponent, PortfolioGroupComponent, PropertyTypeComponent,
     PortfolioDetailComponent, ListTradesComponent,
     ListGroupsComponent, BreadcrumbsComponent, PropertyComponent,
